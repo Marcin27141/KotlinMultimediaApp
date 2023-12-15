061fd9bdfbb04c09ead679f6bb80247ca23d88e7
Marcin27141 <marcin.tkocz@op.pl>
2023-12-15 17:44:40 +0100

    requesting permissions, reading files directories

--- app/src/main/java/com/example/list3/MainActivity.kt
+++ app/src/main/java/com/example/list3/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.list3
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -16,6 +22,35 @@ import com.google.android.material.navigation.NavigationView
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private val REQUEST_CODE = 313;
+    private val REQUIRED_PERMISSIONS = mutableListOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.READ_EXTERNAL_STORAGE)
+        .apply {
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }.toTypedArray()
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(this, "Permissions not granted.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +72,9 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        if (!allPermissionsGranted())
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -49,7 +87,6 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
 
 
 }
--- app/src/main/java/com/example/list3/PrimaryFragment.kt
+++ app/src/main/java/com/example/list3/PrimaryFragment.kt
@@ -1,13 +1,14 @@
 package com.example.list3
 
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.FileProvider
 import com.example.list3.databinding.FragmentPrimaryBinding
-
-
 
 class PrimaryFragment : Fragment() {
     private lateinit var binding: FragmentPrimaryBinding
@@ -17,6 +18,22 @@ class PrimaryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentPrimaryBinding.inflate(inflater, container, false);
+
+        var dir1 = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val dir2 = Environment.getExternalStorageDirectory()
+        val dir3 = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        val dir4 = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        binding.directory1.text = dir1.path
+        binding.directory2.text = dir2.absolutePath
+        binding.directory3.text = dir3.absolutePath
+        binding.directory4.text = dir4?.absolutePath ?: "nothing"
+
+//        dir4?.let {
+//            val sth = BuildConfig.APPLICATION_ID
+//            val uri = FileProvider.getUriForFile(requireContext(), "${BuildConfig.APPLICATION_ID}.provider", it)
+//        }
+
         return binding.root
     }
 
