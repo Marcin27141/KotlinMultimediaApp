e40908f1e4d58ce97d3289599ca5f84d9dd5ad18
Marcin27141 <marcin.tkocz@op.pl>
2023-12-16 10:50:29 +0100

    added file paths, displaying them on main fragment

--- app/src/main/java/com/example/list3/PrimaryFragment.kt
+++ app/src/main/java/com/example/list3/PrimaryFragment.kt
@@ -19,20 +19,19 @@ class PrimaryFragment : Fragment() {
     ): View? {
         binding = FragmentPrimaryBinding.inflate(inflater, container, false);
 
-        var dir1 = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val dir1 = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         val dir2 = Environment.getExternalStorageDirectory()
         val dir3 = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
         val dir4 = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
 
-        binding.directory1.text = dir1.path
+        binding.directory1.text = dir1.scheme + ":/" + dir1.path
         binding.directory2.text = dir2.absolutePath
         binding.directory3.text = dir3.absolutePath
         binding.directory4.text = dir4?.absolutePath ?: "nothing"
-
-//        dir4?.let {
-//            val sth = BuildConfig.APPLICATION_ID
-//            val uri = FileProvider.getUriForFile(requireContext(), "${BuildConfig.APPLICATION_ID}.provider", it)
-//        }
+        dir4?.let {
+            val uri = FileProvider.getUriForFile(requireContext(), requireContext().packageName + ".provider", it)
+            binding.directory5.text = uri.scheme + ":/" + uri.path
+        }
 
         return binding.root
     }
